899e45dd1f92892335fa29deab96f4128c1ec736
Guillermo Caracuel <gcaracuel@gmail.com>
2014-07-29 20:06:51 +0200

    Added Centos 7
    Minor fixes to allow ISO images to be previous downloaded for Windows
    Packer

---
+++ scripts/centos7/root_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Basic system preparation
+set -e
+
+# Add here all the packeges you need as base (For Guest Additions)
+sudo yum install -y wget telnet
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y kernel-devel
+# Rpmforge is not working for CentOS 7 yet we'll use EPEL repos
+#sudo rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+#wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+#sudo rpm -i rpmforge-release-0.5.3-1.el7.rf.*.rpm
+#sudo yum --enablerepo rpmforge install -y dkms
+sudo rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
+sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm;
+sudo yum --enablerepo epel install -y dkms
+
+
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
