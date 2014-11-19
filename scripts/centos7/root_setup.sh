9ef9195c798fa388f51862e1f76ff3aca1068de4
Guillermo Caracuel <gcaracuel@gmail.com>
2014-11-19 12:30:14 +0100

    Fixed Centos7 creation scripts

--- scripts/centos7/root_setup.sh
+++ scripts/centos7/root_setup.sh
@@ -57,8 +57,4 @@ rm /home/vagrant/*.iso
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 
-# Remove traces of mac address from network configuration
-sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm /etc/udev/rules.d/70-persistent-net.rules
-
 date > /etc/vagrant_box_build_time
